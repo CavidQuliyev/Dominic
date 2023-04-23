a181096843d2c217f8dcb9d0eda433c14059a011
cavidguliyev <cavidguliyevv@gmail.com>
2023-04-23 17:32:26 +0400

    last version

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,49 @@
-// let tanitim = document.getElementById('tanitim')
-// let baza = [
-//     'WEB DESIGN',
-//     'CREATIVE DESIGN',
-//     'GRAPHIC DESIGN',
-//     'WEB DEVELOPER',
-// ]
-// tanitim.innerHTML = ""
-// for(let i = 0; i < baza.length; ++i){
-//     if(i < baza.length){
-//         tanitim.innerHTML += baza[i]
+let tanitim = document.getElementById('tanitim')
+let baza = [
+    'WEB DESIGN',
+    'CREATIVE DESIGN',
+    'GRAPHIC DESIGN',
+    'WEB DEVELOPER'
+    
+]
+
+
+    
+    for (let i = 0; i < 4; i++) {
         
-//     }
+        // tanitim.innerHTML = ""
+    
+        switch (i) {
+            case 0:
+                tanitim.innerHTML = baza[i]
+                break;
+            case 1:
+                tanitim.innerHTML = baza[i]
+                break;
+                case 2:
+                tanitim.innerHTML = baza[i]
+                break;
+                case 3:
+                tanitim.innerHTML = baza[i]
+                break
+        }
+        // if(i == 0){
+        //     tanitim.innerHTML = baza[i]
+        // }else if(i == 1){
+        //     tanitim.innerHTML = baza[i]
+        // }
+        // else if(i == 2){
+        //     tanitim.innerHTML = baza[i]
+        // }
+        // else if(i == 3){
+        //     tanitim.innerHTML = baza[i]
+        // }
+     
+           
+
+        
+    }
    
-// }
+
+
+
