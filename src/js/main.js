383a8b7d45d9dd9c5dff630905511eb0e9eb93a1
cavidguliyev <cavidguliyevv@gmail.com>
2023-04-26 17:58:45 +0400

    son versiya

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,7 @@
 let tanitim = document.getElementById('tanitim')
+let btn1 = document.getElementById('btn1')
+let btn2 = document.getElementById('btn2')
+let mainboxcontent = document.querySelector('.main-box-content')
 let baza = [
     'WEB DESIGN',
     'CREATIVE DESIGN',
@@ -7,43 +10,110 @@ let baza = [
     
 ]
 
-
-    
-    for (let i = 0; i < 4; i++) {
-        
-        // tanitim.innerHTML = ""
-    
-        switch (i) {
-            case 0:
-                tanitim.innerHTML = baza[i]
-                break;
-            case 1:
-                tanitim.innerHTML = baza[i]
-                break;
-                case 2:
-                tanitim.innerHTML = baza[i]
-                break;
-                case 3:
-                tanitim.innerHTML = baza[i]
-                break
-        }
-        // if(i == 0){
-        //     tanitim.innerHTML = baza[i]
-        // }else if(i == 1){
-        //     tanitim.innerHTML = baza[i]
-        // }
-        // else if(i == 2){
-        //     tanitim.innerHTML = baza[i]
-        // }
-        // else if(i == 3){
-        //     tanitim.innerHTML = baza[i]
-        // }
-     
-           
-
-        
+let i = 0;
+setInterval(() => {
+    if(i==0){
+        tanitim.innerHTML = baza[0];
+        tanitim.classList.add ("section1-div2-p2-copy")
     }
-   
+    else if(i==1){
+        tanitim.innerHTML = baza[1];
+        tanitim.classList.add ("section1-div2-p2-copy")
+    }
+    else if(i==2){
+        tanitim.innerHTML = baza[2];
+        tanitim.classList.add ("section1-div2-p2-copy")
+    }
+    else if(i==3){
+        tanitim.innerHTML = baza[3];
+        tanitim.classList.add ("section1-div2-p2-copy")
+    }
+    i++;
+    if(i==4){
+        i = 0;
+    }
+    
+}, 1000);
+
+
+btn2.addEventListener('click',function(e){
+    mainboxcontent.classList.add('main-box-contentcopy')
+})
+btn1.addEventListener('click',function(e){
+    mainboxcontent.classList.remove('main-box-contentcopy')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// setInterval(() => {
+//     tanitim.innerHTML = ""
+// for(let i = 0; i < baza.length; i++){
+//     if(i == 0){
+//         tanitim.innerHTML += baza[0]
+//     tanitim.classList.add ("section1-div2-p2-copy")
+//     }
+    
+
+// }
+// },1000);
+
+// setInterval(() => {
+    
+// tanitim.innerHTML = ""
+// for(let i = 0; i < baza.length; i++){
+//     if(i == 1){
+//         tanitim.innerHTML += baza[1]
+//     tanitim.classList.add ("section1-div2-p2-copy")
+//     }
+    
+
+// }
+// }, 2000);
+// // tanitim.classList.remove ("section1-div2-p2-copy")
+// setInterval(() => {
+    
+//     tanitim.innerHTML = ""
+//     for(let i = 0; i < baza.length; i++){
+//         if(i == 2){
+//             tanitim.innerHTML += baza[2]
+//         tanitim.classList.add ("section1-div2-p2-copy")
+//         }
+        
+    
+//     }
+//     }, 3000);
+//     setInterval(() => {
+    
+//         tanitim.innerHTML = ""
+//         for(let i = 0; i < baza.length; i++){
+//             if(i == 3){
+//                 tanitim.innerHTML += baza[3]
+//                 tanitim.classList.add ("section1-div2-p2-copy")
+//             }
+           
+        
+//         }
+//         }, 4000);
+
+
+
 
 
 
